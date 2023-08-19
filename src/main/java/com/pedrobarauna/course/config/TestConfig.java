d2ad614bd06e61a869321c582b39906eb14dd65c
Pedro Barauna <pedro.barauna.50@outlook.com>
2023-08-18 22:59:17 -0300

    Payment, one-to-one association

--- src/main/java/com/pedrobarauna/course/config/TestConfig.java
+++ src/main/java/com/pedrobarauna/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedrobarauna.course.entities.Category;
 import com.pedrobarauna.course.entities.Order;
 import com.pedrobarauna.course.entities.OrderItem;
+import com.pedrobarauna.course.entities.Payment;
 import com.pedrobarauna.course.entities.Product;
 import com.pedrobarauna.course.entities.User;
 import com.pedrobarauna.course.entities.enums.OrderStatus;
@@ -84,5 +85,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
